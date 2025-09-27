6985cbeaef4afe4a756888f201ed78a7a4d77d08
Edward Patch <54278802+ShinkuKira21@users.noreply.github.com>
2025-09-27 15:53:48 +0100

    Verification for Google

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-useHead({
-  meta: [
-    {
-      name: 'google-site-verification',
-      content: 'mNrFM8IBhNeeNB438NCJAVcNM21V-RSGwtL4cBkkRmU'
-    }
-  ]
-})
-</script>
-
 <template>
   <div>
     <NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'google-site-verification',
+          content: 'mNrFM8IBhNeeNB438NCJAVcNM21V-RSGwtL4cBkkRmU'
+        }
+      ]
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
