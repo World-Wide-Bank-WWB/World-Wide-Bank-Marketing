971cd6ca3912305d5546cbc9f5e60f0c8fd402a3
Edward Patch <54278802+ShinkuKira21@users.noreply.github.com>
2025-09-27 15:49:55 +0100

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    {
+      name: 'google-site-verification',
+      content: 'mNrFM8IBhNeeNB438NCJAVcNM21V-RSGwtL4cBkkRmU'
+    }
+  ]
+})
+</script>
+
 <template>
   <div>
     <NuxtLayout>
